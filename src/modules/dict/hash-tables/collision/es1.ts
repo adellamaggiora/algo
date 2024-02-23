0f73e228120ada24225e6f4868059731da8c630c
adellamaggiora <a.dellamaggiora@caen.it>
2024-02-23 14:46:39 +0100

    collision work!

--- src/modules/dict/hash-tables/collision/es1.ts
+++ src/modules/dict/hash-tables/collision/es1.ts
@@ -1,31 +1,47 @@
 import { utils } from '../../../../utilities/utils';
 
-// table size
-const m = 11
+const keys = [35, 83, 57, 26, 15, 63, 97, 46];
+const hashTable = new Array(11).fill(null);
+
 // primary hash function
-const h = k => k % m;
+const h = k => k % hashTable.length;
 // secondary hash function (double hasing)
-const h2 = k => 1 + k % (m - 1);
+const h2 = k => 1 + k % (hashTable.length - 1);
 
 /**
  * 
  * @param k Key
+ * @param m The hash table size
  * @returns Collision strategy resolution
  * i is the attempt iteration
  */
-const manageCollision = k => {
+const _collisionManager = (m) => (k) => {
     return {
-        // h(h(k) + i)
-        linearProbing: i => h(h(k) + 1),
-        // h(h(k) + i^2)
-        quadraticProbing: i => h(h(k) + utils.square(i)),
+        // h(k) + i
+        linearProbing: i => (h(k) + i) % m,
+        // h(k) + i^2
+        quadraticProbing: i => (h(k) + utils.square(i)) % m,
         // h(k) + i h2(k)
-        doubleHashing: i => h(k) + i * h2(k)
+        doubleHashing: i => (h(k) + i * h2(k)) % m
     }
 }
 
-const collision1 = manageCollision(34);
 
-const result = collision1.quadraticProbing(3);
 
-console.log(result);
+const collisionManager = _collisionManager(hashTable.length);
+
+
+for (const k of keys) {
+    const manageCollision = collisionManager(k);
+    for (let i = 0; i < hashTable.length; i++) {
+        const hash = manageCollision.doubleHashing(i);
+        if (hashTable[hash] === null) {
+            hashTable[hash] = k;
+            break;
+        }            
+    }   
+}
+
+console.log(hashTable);
+
+
