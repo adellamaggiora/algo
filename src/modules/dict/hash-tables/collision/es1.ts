f6164bd2e63aff67e1536848351a85c93fc8f65f
adellamaggiora <a.dellamaggiora@caen.it>
2024-02-23 15:12:26 +0100

    ok

--- src/modules/dict/hash-tables/collision/es1.ts
+++ src/modules/dict/hash-tables/collision/es1.ts
@@ -1,8 +1,8 @@
 import { utils } from '../../../../utilities/utils';
 
-const keys = [35, 83, 57, 26, 15, 63, 97, 46];
 const hashTable = new Array(11).fill(null);
-
+// key universe
+const keys = [35, 83, 57, 26, 15, 63, 97, 46];
 // primary hash function
 const h = k => k % hashTable.length;
 // secondary hash function (double hasing)
@@ -10,37 +10,39 @@ const h2 = k => 1 + k % (hashTable.length - 1);
 
 /**
  * 
- * @param k Key
- * @param m The hash table size
- * @returns Collision strategy resolution
- * i is the attempt iteration
+ * @param hashTable 
+ * @param keys 
+ * @param h 
+ * @param h2 
+ * @param collisionResolution
  */
-const _collisionManager = (m) => (k) => {
-    return {
+const hasing = (hashTable, keys, h, h2, collisionResolution) => {
+
+    const collisionManager = k => ({
         // h(k) + i
-        linearProbing: i => (h(k) + i) % m,
+        linearProbing: i => (h(k) + i) % hashTable.length,
         // h(k) + i^2
-        quadraticProbing: i => (h(k) + utils.square(i)) % m,
+        quadraticProbing: i => (h(k) + utils.square(i)) % hashTable.length,
         // h(k) + i h2(k)
-        doubleHashing: i => (h(k) + i * h2(k)) % m
+        doubleHashing: i => (h(k) + i * h2(k)) % hashTable.length
+    })
+
+
+    for (const k of keys) {
+        const handleCollision = collisionManager(k)[collisionResolution];
+        for (let i = 0; i < hashTable.length; i++) {
+            const hash = handleCollision(i);
+            if (hashTable[hash] === null) {
+                hashTable[hash] = k;
+                break;
+            }            
+        }   
     }
+
 }
 
+hasing(hashTable, keys, h, h2, 'linearProbing');
 
-
-const collisionManager = _collisionManager(hashTable.length);
-
-
-for (const k of keys) {
-    const manageCollision = collisionManager(k);
-    for (let i = 0; i < hashTable.length; i++) {
-        const hash = manageCollision.doubleHashing(i);
-        if (hashTable[hash] === null) {
-            hashTable[hash] = k;
-            break;
-        }            
-    }   
-}
 
 console.log(hashTable);
 
