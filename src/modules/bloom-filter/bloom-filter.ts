c061d24904a91a2fb7f8de3447ac1b88bc40e4c3
adellamaggiora <a.dellamaggiora@caen.it>
2024-02-29 15:12:31 +0100

    bllom filters code scaffolding

---
+++ src/modules/bloom-filter/bloom-filter.ts
@@ -0,0 +1,11 @@
+// https://en.wikipedia.org/wiki/Bloom_filter
+
+// inserisce un elemento nel bloom filter
+// insert(x) -> void
+
+// controlla se x è un elemento del bloom filter e risponde
+// false se x sicuramente non è presente
+// true se x può essere presente oppure no
+// lookup(x) -> boolean
+
+// 3 funzioni di hash che vengono mappate in un array di bit (0/1)
